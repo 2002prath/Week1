b877893623b8ee3b5f75b360291bb91d5eaf3cff
2002prath <prathammishra1111@gmail.com>
2025-01-14 16:19:46 +0530

    youngest code added

---
+++ Level2/Youngest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner; 
+ 
+
+class Youngest 
+{  
+   public static void youngest(int N1,int N2,int N3)
+   { 
+    if(N1<N2 && N1<N3) 
+       System.out.println("Yes,Amar  is a youngest person"); 
+       else if(N1>N2 && N2<N3) 
+       System.out.println("Yes,Akbar  is a youngest person"); 
+       else 
+       System.out.println("Yes,Anthony  is a youngest person"); 
+   }
+  
+  public static void main(String[] mmg)
+    {   
+      Scanner sc = new Scanner(System.in); 
+       int N1 = sc.nextInt(); 
+       int N2 = sc.nextInt(); 
+       int N3 = sc.nextInt(); 
+
+       youngest(N1,N2,N3);
+     
+       sc.close();     
+    } 
+}  
