50f1319b2772891786cf784812129ea95678852a
2002prath <prathammishra1111@gmail.com>
2025-01-16 02:02:46 +0530

    Exception added

---
+++ Level1/Exception.java
@@ -0,0 +1,13 @@
+class Exception 
+{ 
+  public static void generate()
+   { 
+      String text = null; 
+      System.out.println(text.length()); 
+   } 
+   
+    public static void main(String[] amg)
+     { 
+       generate(); 
+      } 
+ } 
