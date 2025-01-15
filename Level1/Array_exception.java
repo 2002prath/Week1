a499244cb5ee75dc4c8d1267cbd34038b08403e9
2002prath <prathammishra1111@gmail.com>
2025-01-16 03:31:29 +0530

    array index out of bound exception

---
+++ Level1/Array_exception.java
@@ -0,0 +1,10 @@
+class Array_exception 
+{                                            //Array index out of bound exception is generated 
+  public static void main(String[] mmg)
+   { 
+     int[]  a = new int[5]; 
+       a = "mohan";  
+     
+     System.out.println("Array index out of bound exception"+ a[6]); 
+   }
+}
